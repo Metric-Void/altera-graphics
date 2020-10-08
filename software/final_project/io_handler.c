ef4bab661df42f900b9d916af77c0dccdd950003
Metric-Void <metricvoidlx@gmail.com>
2020-10-08 00:34:41 -0500

    Code cleanup

--- software/final_project/io_handler.c
+++ software/final_project/io_handler.c
@@ -4,35 +4,22 @@
 
 void IO_init(void)
 {
-	printf("Check A");
 	*otg_hpi_reset = 1;
-	printf("Check B");
 	*otg_hpi_cs = 1;
-	printf("Check C");
 	*otg_hpi_r = 1;
-	printf("Check D");
 	*otg_hpi_w = 1;
-	printf("Check E");
 	*otg_hpi_address = 0;
-	printf("Check F");
 	*otg_hpi_data = 0;
+
 	// Reset OTG chip
 	*otg_hpi_cs = 0;
-	printf("Check G");
 	*otg_hpi_reset = 0;
-	printf("Check H");
-	*otg_hpi_reset = 1;
-	printf("Check I");
+	*otg_hpi_reset = 1
 	*otg_hpi_cs = 1;
 }
 
 void IO_write(alt_u8 Address, alt_u16 Data)
 {
-//*************************************************************************//
-//									TASK								   //
-//*************************************************************************//
-//							Write this function							   //
-//*************************************************************************//
 	*otg_hpi_address = Address;
 	*otg_hpi_data = Data;
 	*otg_hpi_cs = 0;
@@ -45,11 +32,6 @@ void IO_write(alt_u8 Address, alt_u16 Data)
 alt_u16 IO_read(alt_u8 Address)
 {
 	alt_u16 temp;
-//*************************************************************************//
-//									TASK								   //
-//*************************************************************************//
-//							Write this function							   //
-//*************************************************************************//
 	*otg_hpi_address = Address;
 	*otg_hpi_cs = 0;
 	*otg_hpi_r = 0;
